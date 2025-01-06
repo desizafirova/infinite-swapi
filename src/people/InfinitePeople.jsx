9d6babc002ccc3082618d154370fbae347ffe3e5
DesiZafirova <desszafirowa@gmail.com>
2025-01-06 13:16:53 +0200

    write useInfiniteQuery call

--- src/people/InfinitePeople.jsx
+++ src/people/InfinitePeople.jsx
@@ -1,13 +1,20 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Person } from './Person';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = 'https://swapi.dev/api/people/';
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery({
+    queryKey: ['sw-people'],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
   return <InfiniteScroll />;
 }
